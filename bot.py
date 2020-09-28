abcd8e861b16b979ad995148dacd73f9f70801bf
LeoDeg <leodeg.dev@gmail.com>
2020-09-28 21:39:23 +0300

    update

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
-import json
-import requests
 import config
-
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
+logger = logging.getLogger(__name__)
 TOKEN = config.token
 
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
@@ -29,36 +29,36 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
 def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-    
+
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    
+
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    
+
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
-    
+
     # log all errors
     dp.add_error_handler(error)
-    
+
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    
-    updater.bot.setWebhook('https://leodegtelegrambot.herokuapp.com/' + TOKEN)
-    
+    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-
 if __name__ == '__main__':
     main()
-
